84d27e679fe5e858318eebbde86f0d255117b52e
Vianney Foucault <vianney.foucault@gmail.com>
2019-01-29 17:57:15 +0100

    Deps update

--- clickhouse_exporter.go
+++ clickhouse_exporter.go
@@ -6,7 +6,7 @@ import (
 	"net/url"
 	"os"
 
-	"./exporter"
+	"exporter"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/log"
